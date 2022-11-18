9eae0ab6caefadb1def8276b90b5d9a102f14e8f
rosalind-carey <110494609+rosalind-carey@users.noreply.github.com>
2022-11-18 13:17:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ with open("sample.txt") as f:
   #you can specify the amount of characters read by your code by passing a number through the read() function
   #change your code to run the first 50 characters of the file
   
-  f_contents = f.read(20)
+  #f_contents = f.read(20)
   #print the first 20 charcters and end the string with *
-  print(f_contents, end = "*")
+  #print(f_contents, end = "*")
   
-  f_contents = f.read(20)
-  print(f_contents, end = "*")
+  #f_contents = f.read(20)
+  #print(f_contents, end = "*")
   
   #use the seek() function to print the first 20 characters
   #skip to the 40th character
